15478f331a49036632910148cd84e4385ab2783c
Myat Kaung Khant <myatkaungkhant@gicjp.com>
2025-07-29 10:43:11 +0900

    start furniture project

---
+++ Furniture/src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
